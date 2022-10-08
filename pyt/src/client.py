24991e949c813a75a07eaab882d3de9377098c2c
Felipe <fpachecomanoel@uol.com.br>
2022-10-08 15:22:31 -0300

    added method to run requests

--- pyt/src/client.py
+++ pyt/src/client.py
@@ -1,8 +1,17 @@
 import json
 import requests
+import time
 def get_request(url):
     headers={'content_types':'application/json'}
     r=requests.get(url,headers=headers)
     print(r.status_code)
     print(r.json())
+
+def run_requests(url):
+    N=1000
+    for i  in range(N):
+        get_request(url)
+    time.sleep(30)
+    for i in range(N):
+        get_request(url)
 
